2dda5a19e125d4009a24d8c16b892cdc0165bfb0
vinaya67 <35846196+vinaya67@users.noreply.github.com>
2018-11-04 00:11:34 +0530

    Update pali.c
    madified pali

--- pali.c
+++ pali.c
@@ -7,7 +7,7 @@ scanf("%d", &n);
 n1 = n; 
 /* logic */ 
 while (n > 0)
-{ rem = n % 10; rev = rev * 10 + rem; n = n / 10; } 
+{ rem = n % 10; rev = rev * 10 + rem; n = n / 10; } #reverse pali
 if (n1 == rev)
 { 
 printf("Given number is a palindromic number"); 
